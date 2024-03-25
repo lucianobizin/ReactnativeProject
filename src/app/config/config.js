0f015a4eac1e9dfccf8946963c14af0d606e3709
LucianoBizin <lucianobizin@gmail.com>
2024-03-25 18:41:05 +0100

    Update config.js

--- src/app/config/config.js
+++ src/app/config/config.js
@@ -2,12 +2,12 @@ const config =  {
 
 
     Frutizia: {
-        FRUTIZIA_BASE_URL: "https://frutizia-default-rtdb.europe-west1.firebasedatabase.app"
+        FRUTIZIA_BASE_URL: "https://frutizia....app"
     },
 
     Google: {
-        GOOGLE_API: "AIzaSyDzvu9GMSEXVlYkSpEx_cl8z9s3vS3RYCY",
-        GOOGLE_GEOLOCALIZATION: "AIzaSyCg5Q3BTNhlbD7_gwUC767Ig8H-eLYOyLo"
+        GOOGLE_API: "COMPLETAR",
+        GOOGLE_GEOLOCALIZATION: "COMPLETAR"
     }
     
 
