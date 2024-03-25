d6300c8a2f09cb99bdac10efc220e06fc8587b94
lucianobizin <lucianobizin@gmail.com>
2024-03-25 15:47:17 +0100

    Subiendo la app completa

---
+++ src/app/config/config.js
@@ -0,0 +1,16 @@
+const config =  {
+
+
+    Frutizia: {
+        FRUTIZIA_BASE_URL: "COMPLETAR"
+    },
+
+    Google: {
+        GOOGLE_API: "COMPLETAR",
+        GOOGLE_GEOLOCALIZATION: "COMPLETAR"
+    }
+    
+
+}
+
+export default config
